5633e935d87059cfd8ca751e44b08efa0a36b6e8
GeoDeli <delimangeorgiana@yahoo.ro>
2021-09-22 18:51:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Farmacie
+ Sa se creeze o mini-aplicatie pentru gestiunea stocurilor de medicamente ale farmaciilor din cele 4 orase disponibile: Bucuresti, Cluj, Timisoara si Iasi.
---
+++ src/farmacie/Farmacie.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmacie;
+
+/**
+ *
+ * @author delim
+ */
+public class Farmacie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AfisareOrasFarmacii a=new AfisareOrasFarmacii();
+        a.setVisible(true);
+    }
+    
+}
