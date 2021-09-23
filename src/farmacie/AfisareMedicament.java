cb55dec74cf8be6f58142d71fc5d3dd77d4744bc
GeoDeli <delimangeorgiana@yahoo.ro>
2021-09-23 14:42:43 +0300

    Afisare medicament din oras
    S-a creat o noua functionalitate: afisarea stocurilor unui medicament.
    Pentru aflare se foloseste o functie ce este apelata de fiecare dintre cele 4 radio Button

--- src/farmacie/AfisareMedicament.java
+++ src/farmacie/AfisareMedicament.java
@@ -30,11 +30,7 @@ public class AfisareMedicament extends javax.swing.JFrame {
         initComponents();
         
     DefaultComboBoxModel  model=new DefaultComboBoxModel();
-
-    
-        
-      
-        
+DefaultComboBoxModel  model1=new DefaultComboBoxModel();
         try{        //realizeaza conexiunea conexiunea la baza de date
         String database="jdbc:mysql://localhost:3306/farmacie";
         String username="root";
@@ -47,9 +43,12 @@ public class AfisareMedicament extends javax.swing.JFrame {
         while(resultSet.next())
         {
             model.addElement(resultSet.getString("Nume"));
+             model1.addElement(resultSet.getString("Nume"));
         }
         SelecteMedicament.setModel(model);
+        MedicamentPerOras.setModel(model1);
           AutoCompleteDecorator.decorate(SelecteMedicament);
+          AutoCompleteDecorator.decorate(MedicamentPerOras);
         }catch(Exception e)
         { 
             System.out.println(e);
@@ -62,11 +61,20 @@ public class AfisareMedicament extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         SelecteMedicament = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         MedicamentInStoc = new javax.swing.JList<>();
         jLabel2 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        MedInOras = new javax.swing.JList<>();
+        jLabel3 = new javax.swing.JLabel();
+        RBBucuresti = new javax.swing.JRadioButton();
+        RBCluj = new javax.swing.JRadioButton();
+        RBTimisoara = new javax.swing.JRadioButton();
+        RBIasi = new javax.swing.JRadioButton();
+        MedicamentPerOras = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -91,26 +99,84 @@ public class AfisareMedicament extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(MedicamentInStoc);
 
-        jLabel2.setText("jLabel2");
+        jLabel2.setText("Lista farmaciilor ce au medicamentul selectat");
+
+        MedInOras.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(MedInOras);
+
+        jLabel3.setText("Selecteaza medicamentul");
+
+        buttonGroup1.add(RBBucuresti);
+        RBBucuresti.setSelected(true);
+        RBBucuresti.setText("Bucuresti");
+        RBBucuresti.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RBBucurestiActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(RBCluj);
+        RBCluj.setText("Cluj");
+        RBCluj.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RBClujActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(RBTimisoara);
+        RBTimisoara.setText("Timisoara");
+        RBTimisoara.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RBTimisoaraActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(RBIasi);
+        RBIasi.setText("Iasi");
+        RBIasi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RBIasiActionPerformed(evt);
+            }
+        });
+
+        MedicamentPerOras.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addGap(27, 27, 27)
+                        .addComponent(SelecteMedicament, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addGap(27, 27, 27)
-                                .addComponent(SelecteMedicament, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(33, 33, 33)
-                        .addComponent(jLabel2)))
-                .addContainerGap(106, Short.MAX_VALUE))
+                                .addComponent(RBBucuresti)
+                                .addGap(18, 18, 18)
+                                .addComponent(RBCluj))
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(RBTimisoara)
+                                .addGap(18, 18, 18)
+                                .addComponent(RBIasi))
+                            .addComponent(MedicamentPerOras, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jLabel2)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(109, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(80, 80, 80))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -119,11 +185,23 @@ public class AfisareMedicament extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(SelecteMedicament, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(34, 34, 34))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(MedicamentPerOras, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(RBBucuresti)
+                    .addComponent(RBCluj)
+                    .addComponent(RBTimisoara)
+                    .addComponent(RBIasi))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -137,39 +215,85 @@ public class AfisareMedicament extends javax.swing.JFrame {
     private void SelecteMedicamentPopupMenuWillBecomeInvisible(javax.swing.event.PopupMenuEvent evt) {//GEN-FIRST:event_SelecteMedicamentPopupMenuWillBecomeInvisible
          //afiseaza farmaciile care au in stoc medicamentul selectat
         try {
-            String med=String.valueOf(SelecteMedicament.getSelectedItem());
-            String cod_med=String.valueOf(SelecteMedicament.getSelectedIndex()+1);
-           String query="Select * from Medicamente where Nume=\""+med+"\""; //querry
-         Statement statement=con.createStatement();  
-         ResultSet resultSet = statement.executeQuery(query); 
-         ArrayList farmacii=new ArrayList(); //unde se stocheaza valorile
         
-         while(resultSet.next())
-         {
-             
+            String cod_med=String.valueOf(SelecteMedicament.getSelectedIndex()+1); //preia codul medicamentului cautat
+             ArrayList farmacii=new ArrayList(); //unde se stocheaza rezultatul
+
+         //cauta in stoc medicamentul cu codul curent
               String q="Select * from Stoc where Cod_Med=\""+cod_med+"\"";
               Statement st=con.createStatement();
               ResultSet rs=st.executeQuery(q);
               while(rs.next())
-              {
+              {//preia informatiile despre farmacie
                   String q2="Select * from `farmacie-tab` where Cod_F=\""+rs.getString("Cod_F")+"\"";
                   Statement s=con.createStatement();
                   ResultSet rs2=s.executeQuery(q2);
                   while(rs2.next())
-                  {
+                  {//stocheaza informatiile ce vor fi afisate utilizatorului
                       String nume=rs2.getString("Nume");
                       String oras=rs2.getString("Oras");
                       farmacii.add(nume+" "+oras);
                   }
               }
-         }
+         
          afiseaza(farmacii,MedicamentInStoc);
 
      } catch (SQLException ex) {
                          Logger.getLogger(AfisareOrasFarmacii.class.getName()).log(Level.SEVERE, null, ex);
      }
     }//GEN-LAST:event_SelecteMedicamentPopupMenuWillBecomeInvisible
+ 
+        //afiseaza cantitatile de medicament din orasul Bucuresti
+    private void RBBucurestiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RBBucurestiActionPerformed
+       CautaInOras("\"Bucuresti\"");
+    }//GEN-LAST:event_RBBucurestiActionPerformed
 
+    private void RBTimisoaraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RBTimisoaraActionPerformed
+        // TODO add your handling code here:
+      CautaInOras("\"Timisoara\"");
+    }//GEN-LAST:event_RBTimisoaraActionPerformed
+
+    private void RBClujActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RBClujActionPerformed
+        // TODO add your handling code here:
+        CautaInOras("\"Cluj\"");
+    }//GEN-LAST:event_RBClujActionPerformed
+
+    private void RBIasiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RBIasiActionPerformed
+        // TODO add your handling code here:
+        CautaInOras("\"Iasi\"");
+    }//GEN-LAST:event_RBIasiActionPerformed
+
+    void CautaInOras(String oras)
+    {
+           String cod_med=String.valueOf(MedicamentPerOras.getSelectedIndex()+1); //preia codul medicamentului cautat
+             ArrayList farmacii=new ArrayList(); //unde se stocheaza rezultatul
+        
+        try {
+             //cauta in stoc medicamentul cu codul curent
+              String q="Select * from Stoc where Cod_Med=\""+cod_med+"\"";
+              Statement st=con.createStatement();
+              ResultSet rs=st.executeQuery(q);
+              while(rs.next())
+              {
+                  String query="Select * from `farmacie-tab` where oras="+oras+" and Cod_F=\""+rs.getString("Cod_F")+"\""; //querry
+                  Statement statement=con.createStatement();  
+                  ResultSet resultSet = statement.executeQuery(query); 
+   
+         while(resultSet.next())
+         {
+             String nume=resultSet.getString("nume"); //preia doar campul ce ne intereseaza
+             String bucati=rs.getString("Cantitate");
+              farmacii.add(nume+" "+bucati+" bucati");     
+         }
+              }
+            
+        afiseaza(farmacii,MedInOras);
+
+     } catch (SQLException ex) {
+                        Logger.getLogger(AfisareOrasFarmacii.class.getName()).log(Level.SEVERE, null, ex);
+     }
+    }
+    
     void afiseaza(ArrayList<String> linii, JList Lista)
 	{
             Lista.removeAll();
@@ -214,10 +338,19 @@ public class AfisareMedicament extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JList<String> MedInOras;
     private javax.swing.JList<String> MedicamentInStoc;
+    private javax.swing.JComboBox<String> MedicamentPerOras;
+    private javax.swing.JRadioButton RBBucuresti;
+    private javax.swing.JRadioButton RBCluj;
+    private javax.swing.JRadioButton RBIasi;
+    private javax.swing.JRadioButton RBTimisoara;
     private javax.swing.JComboBox<String> SelecteMedicament;
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
 }
